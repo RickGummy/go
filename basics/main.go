2fbec85ada09aa595e2057d41d45474890c1210f
RickGummy <xiufupeng@gmail.com>
2026-05-05 12:28:08 -0500

    the tour of go shit

---
+++ basics/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+/*
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
+}
+*/
+
+var c, python, java bool
+
+func main() {
+	var i int
+	fmt.Println(i, c, python, java)
+}
