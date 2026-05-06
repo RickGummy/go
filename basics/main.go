6cc373ae402cbd6252c9f9aedbed7ba74084379d
RickGummy <xiufupeng@gmail.com>
2026-05-05 19:31:42 -0500

    some stuff

--- basics/main.go
+++ basics/main.go
@@ -12,9 +12,21 @@ func split(sum int) (x, y int) {
 }
 */
 
-var c, python, java bool
+// var i, j int = 1, 2
 
 func main() {
+	const a int = 0
+	var b float64
+	var c bool
+	var d string
+
+	fmt.Printf("%v %v %v %q\n", a, b, c, d)
+
 	var i int
-	fmt.Println(i, c, python, java)
+	j := 2.0
+	c = true
+	python := false
+	java := "no!"
+
+	fmt.Println(i, j, c, python, java)
 }
