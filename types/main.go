c46e24d71eec88f173d63285f4452da59f513398
RickGummy <xiufupeng@gmail.com>
2026-05-07 14:18:03 -0500

    IM TOO LAZY TO WRITE ONE SO THIS IS AN AUTO COMMIT, THANKS MATTHEW FOR THE INSPIRATION! LOL

--- types/main.go
+++ types/main.go
@@ -2,36 +2,45 @@ package main
 
 import "fmt"
 
-// type Vertex struct {
-// 	X int
-// 	Y int
-// }
+// fibonacci is a function that returns
+// a function that returns an int.
+func fibonacci() func() int {
+	first, second := 0, 1
 
-// var (
-// 	v1 = Vertex{1, 2}
-// 	v2 = Vertex{X: 1}
-// 	v3 = Vertex{}
-// 	p  = &Vertex{1, 2}
-// )
+	return func() int {
+		result := first
+		first, second = second, first+second
+		return result
+	}
+}
 
 func main() {
-	q := []int{2, 3, 5, 7, 11, 13}
-	fmt.Println(q)
-
-	r := []bool{true, false, true, true, false, true}
-	fmt.Println(r)
-
-	s := []struct {
-		i int
-		b bool
-	}{
-		{2, true},
-		{3, false},
-		{5, true},
-		{7, true},
-		{11, false},
-		{13, true},
+	f := fibonacci()
+	for range 10 {
+		fmt.Println(f())
 	}
-
-	fmt.Println(s)
 }
+
+// package main
+//
+// import (
+// 	"fmt"
+// )
+//
+// func adder() func(int) int {
+// 	sum := 0
+// 	return func(x int) int {
+// 		sum += x
+// 		return sum
+// 	}
+// }
+//
+// func main() {
+// 	pos, neg := adder(), adder()
+// 	for i := 0; i < 10; i++ {
+// 		fmt.Println(
+// 			pos(i),
+// 			neg(-2*i),
+// 		)
+// 	}
+// }
