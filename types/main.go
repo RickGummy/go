2dfbd5964ea796d53a4c9fb14728d702f55eb106
RickGummy <xiufupeng@gmail.com>
2026-05-06 21:19:00 -0500

    IM TOO LAZY TO WRITE ONE SO THIS IS AN AUTO COMMIT, THANKS JIAS FOR THE INSPIRATION LOL

--- types/main.go
+++ types/main.go
@@ -2,17 +2,36 @@ package main
 
 import "fmt"
 
+// type Vertex struct {
+// 	X int
+// 	Y int
+// }
+
+// var (
+// 	v1 = Vertex{1, 2}
+// 	v2 = Vertex{X: 1}
+// 	v3 = Vertex{}
+// 	p  = &Vertex{1, 2}
+// )
+
 func main() {
-	i, j := 42, 2701
+	q := []int{2, 3, 5, 7, 11, 13}
+	fmt.Println(q)
 
-	p := &i
-	fmt.Println(*p)
+	r := []bool{true, false, true, true, false, true}
+	fmt.Println(r)
 
-	*p = 21
-	fmt.Println(i)
+	s := []struct {
+		i int
+		b bool
+	}{
+		{2, true},
+		{3, false},
+		{5, true},
+		{7, true},
+		{11, false},
+		{13, true},
+	}
 
-	p = &j
-	*p = *p / 37
-
-	fmt.Println(j)
+	fmt.Println(s)
 }
